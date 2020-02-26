c9a3c877ddbe786cfabf0c110cc62353ca805016
Dilruba Khan <rubatanisa@yahoo.ie>
2020-02-26 14:42:29 +0000

    Create post and category

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+
+	def index
+		@firstContent = "One of the reasons that bad boys are so attractive in the first place is that they exude magnetic confidence."
+		@secondContent = "Sometimes when we court relationships with a lot of conflict because conflict is distracting. "
+	end
+
+	def new
+	
+	end
+
+	def create
+	
+	end
+
+	def edit
+	
+	end
+
+	def update
+	
+	end
+	def show
+	
+	end
+	def destroy
+	
+	end
+end
