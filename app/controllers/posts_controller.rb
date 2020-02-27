dcf6fae0c2120eda7d01dee8bf21734f76e4302b
Dilruba Khan <rubatanisa@yahoo.ie>
2020-02-27 23:53:50 +0000

    Implement update and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,24 +5,52 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
+		@categorys = Category.all
 	
 	end
 
 	def create
+		@post =  Post.create(post_params)
+		if @post.save
+			redirect_to posts_path, :notice => "Your post has been saved"
+		else
+			render "new"
+		end
 	
 	end
 
 	def edit
-	
+		@post = Post.find(params[:id])
 	end
 
 	def update
-	
+		@post = Post.find(params[:id])
+
+		if @post.update_attributes(post_params)
+			redirect_to post_path, :notice => "Your post has been updated"
+		else
+			render "edit"
+		end
 	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
 	def destroy
-	
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "Your post has been deleted"	
 	end
+
+	 private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+   def post_params
+      params.require(:post).permit(:title, :body, :category_id)
+    end
+	
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+//= require jquery
+//= require materialize-sprockets
